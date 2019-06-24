8b10c7ea9ef5f6530d2f21aeccb347e7b3d8386e
Dillon Huff <dillonhuff@gmail.com>
2019-06-23 22:29:02 -0700

    Initial commit

---
+++ axi_slave_ram.v
@@ -0,0 +1,82 @@
+module axi_slave_ram(
+
+                     // Global signals
+                     input                         aclk,
+                     input                         aresetn,
+
+                     // Write address channel
+                     input [ADDRESS_WIDTH - 1 : 0] awaddr,
+                     input [7:0]                   awlen,
+                     input [2:0]                   awsize,
+                     input [1:0]                   awburst,
+                     input                         awvalid,
+                     output                        awready,
+
+                     // Write data channel
+                     input [DATA_WIDTH - 1 : 0]    wdata,
+                     input [STROBE_WIDTH - 1 : 0]  wstrb,
+                     input                         wlast,
+                     input                         wvalid,
+                     output                        wready,
+
+                     // Write response channel
+                     output [1:0]                  bresp,
+                     output                        bvalid,
+                     input                         bready,
+
+                     // Read address channel
+                     input [ADDRESS_WIDTH - 1 : 0] araddr,
+                     input [7:0]                   arlen,
+                     input [2:0]                   arsize,
+                     input [1:0]                   arburst,
+                     input                         arvalid,
+                     output                        arready,
+
+                     // Read data channel
+                     output [DATA_WIDTH - 1 : 0]   rdata,
+                     output [1:0]                  rresp,
+                     output                        rlast, 
+                     output                        rvalid,
+                     input                         rready
+                     );
+
+   parameter DATA_WIDTH = 32;
+   parameter STROBE_WIDTH = DATA_WIDTH / 8;
+   parameter ADDRESS_WIDTH = 8;
+   parameter BYTES_PER_WORD = STROBE_WIDTH;
+
+   // Start with slave reads, take in read burst, and then
+   // emit read data?
+
+   // What is the size of the underlying memory?
+   // 2**(address width) bytes, so
+   // 2**(address width) / bytes_per_word
+
+   //reg [DATA_WIDTH - 1 : 0]                        ram [(2**(ADDRESS_WIDTH)) / BYTES_PER_WORD - 1: 0];
+
+   reg [7:0]                                       ram[2**ADDRESS_WIDTH];
+   
+   // Read state is?
+   // Idle (waiting for burst)
+   // Servicing burst
+
+   localparam READ_CONTROLLER_WAITING = 0;
+   localparam READ_CONTROLLER_ACTIVE = 1;   
+
+   reg                                             read_state;
+
+   reg [ADDRESS_WIDTH - 1 : 0]                        read_burst_base_addr;
+   reg [8:0]                                       read_bursts_left;
+   reg [2:0]                                       read_burst_size;   
+   reg [1:0]                                       read_burst_type;
+   
+   always @(posedge aclk) begin
+      if (!aresetn) begin
+         read_state <= READ_CONTROLLER_WAITING;
+      end else begin
+         
+      end
+   end
+   
+   
+endmodule
